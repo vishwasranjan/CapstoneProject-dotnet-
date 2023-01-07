d352a069e42c4758065abca0c48c693f7953a104
Susree <susree.sadangi@happiestminds.com>
2023-01-07 16:36:38 +0530

    test

--- Backend/BankDBFirstLib/DataAccessLayer.cs
+++ Backend/BankDBFirstLib/DataAccessLayer.cs
@@ -1,18 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Transactions;
-
-namespace BankDBFirstLib
-{
-    public class DataAccessLayer : IDatabase
-    {
-        private readonly BankDBContext dbCtx;
-        //dependency injection
+﻿Loan
         public DataAccessLayer(BankDBContext dbCtx)
         {
             this.dbCtx = dbCtx;
